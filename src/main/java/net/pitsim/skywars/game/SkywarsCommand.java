b8ba12fb8f9d1347837f7b1243e9201c3e9bf10f
KyroKrypt <kyrokrypt@gmail.com>
2023-03-23 20:31:16 -0700

    fix main command

--- src/main/java/net/pitsim/skywars/game/SkywarsCommand.java
+++ src/main/java/net/pitsim/skywars/game/SkywarsCommand.java
@@ -2,47 +2,35 @@ package net.pitsim.skywars.game;
 
 import dev.kyro.arcticapi.misc.AOutput;
 import net.pitsim.skywars.PitSim;
-import net.pitsim.skywars.controllers.CombatManager;
-import net.pitsim.skywars.controllers.DamageManager;
-import net.pitsim.skywars.controllers.SpawnManager;
-import net.pitsim.skywars.controllers.objects.PitEnchant;
-import net.pitsim.skywars.controllers.objects.PitPlayer;
-import net.pitsim.skywars.events.AttackEvent;
-import net.pitsim.skywars.events.OofEvent;
-import net.pitsim.skywars.misc.Sounds;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 
 public class SkywarsCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-
-		if(args.length < 1) return false;
-
-		if(args[0].equals("load")) {
-			PitSim.INSTANCE.onInit();
-		}
-
-
 		if(!(sender instanceof Player)) return false;
 		Player player = (Player) sender;
-		if(!player.isOp()) return false;
 
-		if(args[0].equals("start")) {
+		String command = args[0].toLowerCase();
+		if(command.equals("start")) {
+			if(!player.hasPermission("skywars.start")) {
+				AOutput.send(player, "&c&lERROR!&7 You do not have permission for that");
+				return false;
+			}
+
 			QueueManager.override = true;
 			QueueManager.countdown();
 			QueueManager.setSeconds(10);
 			QueueManager.setMinutes(0);
+		}
+
+		if(!player.isOp()) return false;
+
+		if(command.equals("load")) {
+			PitSim.INSTANCE.onInit();
 		}
 
 		return false;
